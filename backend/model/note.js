898b1468cf1feb90dcb5fe14737e37c8175519a2
Lovehiku <hikulove2@gmail.com>
2025-12-25 00:37:19 +0300

    note schema

---
+++ backend/model/note.js
@@ -0,0 +1,21 @@
+const mongoose = require ('mongoose');
+
+// Define Note Schema
+const noteSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    content:{
+        type:String,
+        required:true
+    }
+},{timestamps:true});
+
+module.exports=mongoose.model('Note',noteSchema);
